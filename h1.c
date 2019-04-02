89ac3c18e6b9f89917a45488793961026a747b43
LEKSHMICHANDRABOSECP <newshadows19@gmail.com>
2019-04-02 14:35:49 +0530

    @nd prgm

---
+++ h1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,j;
+clrscr();
+printf "Enter the Limit:";
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+printf "* \t";
+}
+printf "\n
+}
+getch();
+}
+
